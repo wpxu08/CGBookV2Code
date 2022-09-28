f5890fa981e31e29565e9b36a0a30a8acbe976b3
Xu Wenpeng <wpxu08@gmail.com>
2022-09-28 09:13:46 +0800

    去掉glaux.lib库

--- CGP9-1_Texture/CGP9-1_Texture.cpp
+++ CGP9-1_Texture/CGP9-1_Texture.cpp
@@ -1,55 +1,123 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <GL/glut.h>
-#include<GL/glaux.h>
 #include<stdio.h>
 
-#pragma comment(lib, "glaux.lib")
+#define BMP_Header_Length 54  //图像数据在内存块中的偏移
 
 static GLfloat theta[] = { 0.0,0.0,0.0 };
 static GLint axis = 2;
 GLuint texture[1];    //创建一个纹理空间
-AUX_RGBImageRec* LoadBMP(CHAR* Filename) //载入位图图像
+
+// 函数power_of_two用于判断一个整数是不是2的整数次幂
+int power_of_two(int n)
 {
-	FILE* File = NULL;                             //文件句柄
-	if (!Filename)                                //确保文件名已提供
-	{
-		return NULL;
-	}
-	File = fopen(Filename, "r");                   //尝试打开文件
-	if (File)
-	{
-		fclose(File);                            //关闭文件
-		return auxDIBImageLoadA(Filename);       //载入位图并返回指针
-	}
-	return NULL;                              //如果载入失败，返回NULL
+	if (n <= 0)
+		return 0;
+	return (n & (n - 1)) == 0;
 }
 
-int LoadGLTextures()  //载入位图并转换成纹理
+GLuint LoadTextures(const char* file_name)
 {
-	int Status = FALSE;                         //状态指示器
-	AUX_RGBImageRec* TextureImage[1];         //创建纹理的存储空间
-	memset(TextureImage, 0, sizeof(void*) * 1);//初始化
-//载入位图，检查有无错误，如果位图没找到则退出
-	if (TextureImage[0] = LoadBMP((CHAR*)"texture4.bmp"))
+	GLint width, height, total_bytes;
+	GLubyte* pixels = 0;
+	GLuint last_texture_ID = 0, texture_ID = 0;
+	// 打开文件，如果失败，返回
+	FILE* pFile = fopen(file_name, "rb");
+	if (pFile == 0)
+		return 0;
+	// 读取文件中图象的宽度和高度
+	fseek(pFile, 0x0012, SEEK_SET);
+	fread(&width, 4, 1, pFile);
+	fread(&height, 4, 1, pFile);
+	fseek(pFile, BMP_Header_Length, SEEK_SET);
+	// 计算每行像素所占字节数，并根据此数据计算总像素字节数
 	{
-		Status = TRUE;
-		//使用来自位图数据生成的纹理
-		glBindTexture(GL_TEXTURE_2D, 0);
-		//指定二维纹理
-		glTexImage2D(GL_TEXTURE_2D, 0, 3, TextureImage[0]->sizeX, TextureImage[0]->sizeY, 0, GL_RGB, GL_UNSIGNED_BYTE, TextureImage[0]->data);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		GLint line_bytes = width * 3;
+		while (line_bytes % 4 != 0)
+			++line_bytes;
+		total_bytes = line_bytes * height;
 	}
-
-	if (TextureImage[0])               //纹理是否存在
+	// 根据总像素字节数分配内存
+	pixels = (GLubyte*)malloc(total_bytes);
+	if (pixels == 0)
 	{
-		if (TextureImage[0]->data)     //纹理图像是否存在
+		fclose(pFile);
+		return 0;
+	}
+	// 读取像素数据
+	if (fread(pixels, total_bytes, 1, pFile) <= 0)
+	{
+		free(pixels);
+		fclose(pFile);
+		return 0;
+	}
+	// 对就旧版本的兼容，如果图象的宽度和高度不是的整数次方，则需要进行缩放
+	// 若图像宽高超过了OpenGL规定的最大值，也缩放
+	{
+		GLint max;
+		glGetIntegerv(GL_MAX_TEXTURE_SIZE, &max);
+		if (!power_of_two(width)
+			|| !power_of_two(height)
+			|| width > max
+			|| height > max)
 		{
-			free(TextureImage[0]->data);      //释放纹理图像占用的内存
+			const GLint new_width = 256;
+			const GLint new_height = 256; // 规定缩放后新的大小为边长的正方形
+			GLint new_line_bytes, new_total_bytes;
+			GLubyte* new_pixels = 0;
+			// 计算每行需要的字节数和总字节数
+			new_line_bytes = new_width * 3;
+			while (new_line_bytes % 4 != 0)
+				++new_line_bytes;
+			new_total_bytes = new_line_bytes * new_height;
+			// 分配内存
+			new_pixels = (GLubyte*)malloc(new_total_bytes);
+			if (new_pixels == 0)
+			{
+				free(pixels);
+				fclose(pFile);
+				return 0;
+			}
+			// 进行像素缩放
+			gluScaleImage(GL_RGB,
+				width, height, GL_UNSIGNED_BYTE, pixels,
+				new_width, new_height, GL_UNSIGNED_BYTE, new_pixels);
+			// 释放原来的像素数据，把pixels指向新的像素数据，并重新设置width和height
+			free(pixels);
+			pixels = new_pixels;
+			width = new_width;
+			height = new_height;
 		}
-		free(TextureImage[0]);        //释放图像结构
 	}
-	return Status;                   //返回Status
+	// 分配一个新的纹理编号
+	//glGenTextures(1, &texture_ID);
+	//if (texture_ID == 0)
+	//{
+	//	free(pixels);
+	//	fclose(pFile);
+	//	return 0;
+	//}
+	// 绑定新的纹理，载入纹理并设置纹理参数
+	// 在绑定前，先获得原来绑定的纹理编号，以便在最后进行恢复
+	GLint lastTextureID = last_texture_ID;
+	glGetIntegerv(GL_TEXTURE_BINDING_2D, &lastTextureID);
+	glBindTexture(GL_TEXTURE_2D, texture_ID);
+	//********1.请输入代码设置纹理映射控制函数********//
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	//**********************************************//
+	//*********2.请输入代码设置纹理的定义函数*********//
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0,
+		GL_BGR_EXT, GL_UNSIGNED_BYTE, pixels);
+	//**********************************************//
+	//********3.请输入代码进行纹理映射方式设置*********//
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	//**********************************************//
+	glBindTexture(GL_TEXTURE_2D, lastTextureID);  //恢复之前的纹理绑定
+	free(pixels);
+	return texture_ID;
 }
 
 void display()
@@ -104,7 +172,7 @@ void Init()
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_TEXTURE_2D);
 	glShadeModel(GL_SMOOTH);
-	LoadGLTextures();
+	LoadTextures("texture4.bmp");  //加载纹理
 	GLfloat planes[] = { 0.5,0.0,0.0,0.5 };
 	GLfloat planet[] = { 0.0,0.5,0.0,0.5 };
 	glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
@@ -118,7 +186,6 @@ void Init()
 }
 int main(int argc, char** argv)
 {
-
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(500, 500);
